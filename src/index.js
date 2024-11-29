e6ff16937b5fef728b7b771fcba4c7bf2132540c
NooMaaBee <latteliten@hotmail.com>
2024-11-29 17:27:30 +0100

    added axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function updateWeather(response) {
+  console.log(response);
+}
+
+function searchCity(city) {
+  let apiKey = "11e1c3be102e1o7a295b1f381bf4dtf4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
